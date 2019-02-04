bc1da97a4b90ccdb9a1710e017e66d17518a5c88
Zach Peng <rookie.dev@gmail.com>
2019-02-04 09:35:41 -0500

    add license header

--- array_analysis.py
+++ array_analysis.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb  1 14:48:19 2019
+    Antenna Array Analysis
+    Copyright (C) 2019  Zhengyu Peng
 
-@author: zjx8rj
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import sys
